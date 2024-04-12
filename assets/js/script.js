f8ae549191aec50f4805ff32c243384a38aad4a0
OBMAKF-DEV <140197512+OBMAKF-DEV@users.noreply.github.com>
2024-04-13 06:09:10 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,23 @@ function scrollToSection(sectionId) {
     behavior: 'smooth'
   });
 }
+
+// Typewriter effect
+function typeWriter(element, text, speed)
+  let i = 0;
+  const interval = setInterval(() => {
+    element.textContext += text.charAt(i);
+    i++;
+    if (i > text.length) {
+      clearInterval(interval);
+    }
+  }, speed);
+}
+
+// Window onload typing effect.
+window.onload = function() {
+  const header = document.getElementById('main-title');
+  const text = 'Welcome to brnsb.eth!';
+  const speed = 100;
+  typeWriter(header, text, speed);
+};
