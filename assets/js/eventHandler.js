b799464a2e11b4cbff070db461e89b4859ab920e
Nathan Bransby <v141198@tafe.wa.edu.au>
2024-08-13 04:27:15 +0800

    Create eventHandler.js

---
+++ assets/js/eventHandler.js
@@ -0,0 +1,8 @@
+export function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i=0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
